179ffa87975b39c40878a48cc4c96602b68bc648
adrianbarbosa7C <adrian.barbosa@upr.edu>
2026-03-13 18:11:24 -0400

    Fixed DyEnemy textures to two textures

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -6,11 +6,19 @@ class DyEnemy : public Enemy {
         float angle = 135;
         float aimAngle = 225;
         bool loop = false;
+        Rectangle textureRect;
 
     public:
         DyEnemy(float x, float y) : Enemy(x, y) { 
             this->cooldown = GetRandomValue(90, 300);
-            this->health = 1; 
+            this->health = 1;
+
+            int textureofChoice = GetRandomValue(0, 1);
+            if (textureofChoice == 0){
+                this->textureRect= Rectangle{2, 128, 13, 14};
+            } else{
+                this->textureRect = Rectangle{2, 147, 13, 13};
+            }
         }
 
         void draw() override;
